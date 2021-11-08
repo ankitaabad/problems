8b5fe9bd01f2ea6923ba84329e660bed7b089a12
ankit aabad <ankitaabad.1@gmail.com>
2021-11-08 14:42:39 +0530

    Create delete.js

---
+++ delete.js
@@ -0,0 +1,14 @@
+const delete_items = (arr, indices) => {
+    //write your code here
+
+
+}
+
+const test = () =>{
+    const arr = [1,3,5,7,9,8];
+    const indices = [0,1,3];
+    delete_items(arr,indices);
+    console.log(arr);
+    //should print [5,9,8]
+}
+test(); 
