7e2dca72a4e88491cfe5614790a650f73d337075
ankit aabad <ankitaabad.1@gmail.com>
2021-11-22 14:48:33 +0530

    Create diff.js

---
+++ diff.js
@@ -0,0 +1,13 @@
+const find_diff = ({prev,curr}) => {
+//write your code here
+
+
+}
+
+const test_me = () => {
+  const prev = [1,3,5];
+  const curr = [3,6,7];
+  console.log(find_diff({prev,curr}));
+  //should print { added: [ 6, 7 ], deleted: [ 1, 5 ] }
+}
+test_me();
