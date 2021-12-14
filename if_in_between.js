a70774d541ccc8f7af65dc75aff7d7a6f5d706a9
ankitaabad <ankitaabad.1@gmail.com>
2021-12-14 14:07:35 +0530

    add new problem

---
+++ if_in_between.js
@@ -0,0 +1,8 @@
+const if_in_between = (month,year) => {
+    const from_month = 8, from_year = 2020;
+    const to_month = 3, to_year = 2022;
+    //write your code here.
+}
+
+
+console.log(if_in_between(2,2021));
